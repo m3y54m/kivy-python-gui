34977f7fd57a4bdfeedf586916f3ebf88c9049ae
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-06 05:07:46 +0330

    Define a GridLayout

--- hello.py
+++ hello.py
@@ -4,15 +4,28 @@ kivy.require('2.0.0') # replace with your current kivy version !
 from kivy.app import App
 from kivy.uix.label import Label
 
+from kivy.uix.gridlayout import GridLayout
 
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
+
+# Class name becomes window title
 class HelloWorld(App):
-
     def build(self):
+        # Create a new GridLayout
+        self.window = GridLayout()
+        # Set number of columns
+        self.window.cols = 1
 
+        #Add widgets to window
         self.greeting = Label(text='Hello world!')
-        
-        return self.greeting
+        self.window.add_widget(self.greeting)
+
+        return self.window
 
 
 if __name__ == '__main__':
+    # Run the GUI
     HelloWorld().run()
