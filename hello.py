36dadccc897aedd814a1d2bc91b2597cf0b6af5f
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-10-06 05:33:30 +0330

    Do some styling

--- hello.py
+++ hello.py
@@ -16,21 +16,38 @@ class HelloWorld(App):
         self.window = GridLayout()
         # Set number of columns
         self.window.cols = 1
+        # GridLayout styling
+        self.window.size_hint = (0.6,0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
 
         #Add widgets to window
         # Display image
         self.window.add_widget(Image(source="parrot.jpg"))
 
         # Display text
-        self.greeting = Label(text='Hello world!')
+        self.greeting = Label(
+            text='Hello world!',
+            # Lable styling
+            font_size=18,
+            color="#00FFCE"
+        )
         self.window.add_widget(self.greeting)
 
         # Input text
-        self.user = TextInput(multiline=False)
+        self.user = TextInput(
+            multiline=False,
+            padding_y=(20,20),
+            size_hint=(1,0.5)
+        )
         self.window.add_widget(self.user)
 
         # Add button
-        self.button = Button(text="Say Hello!")
+        self.button = Button(
+            text="Say Hello!",
+            size_hint=(1,0.5),
+            bold=True,
+            background_color="#00FFCE"
+        )
         # Bind the callback function to the button
         self.button.bind(on_press=self.button_callback)
         self.window.add_widget(self.button)
